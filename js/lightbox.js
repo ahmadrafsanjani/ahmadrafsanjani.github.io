7300ac3ba89059989b921d17a44b355937546dd9
Ahmad Rafsanjani <ahmad.rafsanjani@gmail.com>
2020-07-11 16:01:36 +0200

    commit

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,3 +1,14 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
 function is_youtubelink(url) {
     var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
     return (url.match(p)) ? RegExp.$1 : false;
